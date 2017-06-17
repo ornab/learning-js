974600bc03356cb15bb86f97e75e22634a9e6e6b
ornab <hzamil17@gmail.com>
2017-06-17 19:48:08 +0600

    Initial commit

---
+++ js_basics/script.js
@@ -0,0 +1,38 @@
+var name = "Ornab";
+
+console.log("Hello " + name);
+
+
+
+var name = "john"
+
+var age = 26
+
+console.log(name + age)
+console.log(age + age) 
+
+
+var job, isMarried
+
+
+console.log(job)
+
+job = 'programmer'
+isMarried = false
+
+console.log(name + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried + '.')
+
+
+age = 'twenty six'
+job = 'driver'
+
+
+console.log(name + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried + '.')
+
+
+
+var lastName = prompt('What is the last name?')
+
+console.log(lastName)
+
+alert(name + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried + '.' + ' His last name is ' + lastName + '.')
